9c65b33803aca2ff12ee9ebd94704091b33f9441
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2025-08-25 21:39:00 -0700

    consistency change

--- gamemodes/horde/gamemode/status/buff/sv_winds_of_taranis.lua
+++ gamemodes/horde/gamemode/status/buff/sv_winds_of_taranis.lua
@@ -5,8 +5,8 @@ function plyMeta:Horde_AddWindsOfTaranis( duration )
         self:Horde_RemoveWindsOfTaranis()
     end )
 
-    if self.Horde_WindsOfTaranis then return end
-    self.Horde_WindsOfTaranis = true
+    if self.Horde_WindsOfTaranis == 1 then return end
+    self.Horde_WindsOfTaranis = 1
 
     net.Start( "Horde_SyncStatus" )
         net.WriteUInt( HORDE.Status_Winds_of_Taranis, 8 )
@@ -17,8 +17,8 @@ end
 function plyMeta:Horde_RemoveWindsOfTaranis()
     if not self:IsValid() then return end
 
-    if not self.Horde_WindsOfTaranis then return end
-    self.Horde_WindsOfTaranis = false
+    if self.Horde_WindsOfTaranis == 0 then return end
+    self.Horde_WindsOfTaranis = 0
 
     timer.Remove( "Horde_RemoveWindsOfTaranis" .. self:SteamID() )
 
@@ -29,11 +29,11 @@ function plyMeta:Horde_RemoveWindsOfTaranis()
 end
 
 function plyMeta:Horde_GetWindsOfTaranis()
-    return self.Horde_WindsOfTaranis or false
+    return self.Horde_WindsOfTaranis or 0
 end
 
 hook.Add( "Horde_PlayerMoveBonus", "Horde_WindsOfTaranisSpeed", function( ply, bonusWalk, bonusRun, bonusJump )
-    if ply:Horde_GetWindsOfTaranis() then
+    if ply:Horde_GetWindsOfTaranis() == 1 then
         local bonus2 = 0.2 * ( 1 + ply:Horde_GetApplyBuffMore() )
         bonusWalk.increase = bonusWalk.increase + bonus2
         bonusRun.increase = bonusRun.increase + bonus2
@@ -42,11 +42,11 @@ hook.Add( "Horde_PlayerMoveBonus", "Horde_WindsOfTaranisSpeed", function( ply, b
 end )
 
 hook.Add( "Horde_OnPlayerDamageTaken", "Horde_WindsOfTaranisEvasion", function( ply, _, bonus )
-    if ply:Horde_GetWindsOfTaranis() then
+    if ply:Horde_GetWindsOfTaranis() == 1 then
         bonus.evasion = bonus.evasion + 0.20
     end
 end )
 
 hook.Add( "Horde_ResetStatus", "Horde_WindsOfTaranisReset", function( ply )
-    ply.Horde_WindsOfTaranis = false
+    ply.Horde_WindsOfTaranis = 0
 end )
