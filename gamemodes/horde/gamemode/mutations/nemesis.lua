303e0ee8c194213ba7b5ccd42b64a402d1b35134
Alex9914 <77191624+Alex9914@users.noreply.github.com>
2026-02-19 21:43:52 -0800

    refactor(nemesis): replace timer and FindInSphere spam with horde_nemesis_cloud entity

--- gamemodes/horde/gamemode/mutations/nemesis.lua
+++ gamemodes/horde/gamemode/mutations/nemesis.lua
@@ -21,28 +21,10 @@ end
 MUTATION.Hooks.Horde_OnEnemyKilled = function(victim, killer, weapon)
     if victim:Horde_HasMutation("nemesis") then
         local victim_pos = victim:GetPos()
-        for i =0,10 do
-            timer.Simple(0.5 + i * 0.2, function ()
-                local rand = VectorRand()
-                if rand.z < 0 then rand.z = -rand.z end
-                local pos = victim_pos + rand * math.Rand(10, 50)
-                for _, e1 in pairs(ents.FindInSphere(pos, 150)) do
-                    if HORDE:IsPlayerOrMinion(e1) == true then
-                        local dmginfo = DamageInfo()
-                        dmginfo:SetDamage(math.max(5, 0.05 * e1:GetMaxHealth()))
-                        dmginfo:SetAttacker(Entity(0))
-                        dmginfo:SetInflictor(Entity(0))
-                        dmginfo:SetDamagePosition(pos)
-                        dmginfo:SetDamageType(DMG_ACID)
-                        e1:TakeDamageInfo(dmginfo)
-                    end
-                end
-                local e = EffectData()
-                    e:SetOrigin(pos)
-                util.Effect("corruption", e, true, true)
-                sound.Play("ambient/levels/canals/toxic_slime_sizzle2.wav", pos)
-            end)
-        end
+
+        local ent = ents.Create( "horde_nemesis_cloud" )
+        ent:SetPos( victim_pos )
+        ent:Spawn()
     end
 end
 
